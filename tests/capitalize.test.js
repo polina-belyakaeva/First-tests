3f753739f3d11df49b63a878a9079455b2ebcda0
Полина Белякаева <polinabelakaeva@MBP-Polina.modem.local>
2023-02-06 21:29:05 +0300

    Changed to assert

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,8 @@
 import { capitalize } from "../src/capitalize.js";
+import { strict as assert } from 'node:assert';
 
-if (capitalize('hello') !== 'Hello') {
-    throw new Error('Функция работает неверно!');
-  }
+assert(capitalize('') === '');
+
+assert(capitalize('hello') === 'Hello');
   
-  if (capitalize('') !== '') {
-    throw new Error('Функция работает неверно!');
-  }
-  
-  console.log('Все тесты пройдены!');
+console.log('Все тесты пройдены!');
